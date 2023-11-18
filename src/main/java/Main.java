67b35ec5a3c54575c2f0eb27f6039973d16d0629
DenysKavkalo <denysfalcons@gmail.com>
2023-11-18 15:56:51 +0000

    5-Changes on SQLiteWeatherStorage.java and other classes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,8 @@
 import control.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.Scanner;
 
 
 public class Main {
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
